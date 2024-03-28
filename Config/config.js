a7c2bc23a58a8de76dfa0825aa3bcbcb6ea2a82e
bedhouche <anaisbedhouche03@gmail.com>
2024-03-28 21:00:31 +0100

    Add config

---
+++ Config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+ token:  "MTE2OTczNDAyODUyMjQzODcwNg.G-uRIe.sUO-uANfZpu1kDKoiiMBfy0Bzuxan_XCGZDOrc",
+ prefix: '!', // Définissez le préfixe ici
+ clientId:"1169734028522438706",
+ guildId:"1174078075479261206",
+ mongodbUrl: "mongodb://127.0.0.1:27017/student_bot"
+
+};
